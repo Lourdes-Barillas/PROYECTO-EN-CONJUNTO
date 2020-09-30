de8f05d1b03799eb79c037aa75dc461be548dff1
DeLeonEsau <deleonesau29032001@gmail.com>
2020-09-29 22:24:12 -0600

    Reportes
    añadidas las interfaces sin codificar de reportes

---
+++ Proyecto/src/Frames/Reportes/Clientes/ReporteClientes.java
@@ -0,0 +1,14 @@
+package Frames.Reportes.Clientes;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+public class ReporteClientes {
+
+
+    public void menu_Salir(ActionEvent actionEvent) {
+
+
+    }
+}//class
---
+++ Proyecto/src/Frames/Reportes/Productos/ReporteProductos.java
@@ -0,0 +1,4 @@
+package Frames.Reportes.Productos;
+
+public class ReporteProductos {
+}
