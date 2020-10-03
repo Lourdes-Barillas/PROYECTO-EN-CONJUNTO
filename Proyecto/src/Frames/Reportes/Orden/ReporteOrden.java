3dcf4b0661442c0e490da554b2581b6b108ce798
DeLeonEsau <deleonesau29032001@gmail.com>
2020-10-02 19:01:37 -0600

    codificacion en reportes
    se complementaron algunas secciones de codigo

--- Proyecto/src/Frames/Reportes/Orden/ReporteOrden.java
+++ Proyecto/src/Frames/Reportes/Orden/ReporteOrden.java
@@ -4,10 +4,16 @@ import Frames.PruebaVentanas;
 import Frames.Reportes.Reportes;
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import Orden.DataSistema;
+
+import javax.swing.*;
 
 public class ReporteOrden {
+    public TextField id_B;
     private Stage reportes;
+
     public void setControllerReportes(Stage orden) {
         this.reportes = orden;
     }
@@ -16,4 +22,18 @@ public class ReporteOrden {
         Stage stage = (Stage) souce.getScene().getWindow();
         stage.close();
     }
-}
+
+    public void Buscar(ActionEvent event) {
+        String idc=id_B.getText();
+        DataSistema data= new DataSistema();
+        if(data.getOrden().size()==0){
+            JOptionPane.showMessageDialog(null, "na hay registros en existencia");
+        }
+        //------------------------------------------------------------------------
+        //de aqui en adelante el resto en caso que si encuentre registros
+        //con que ma ayudes a arreglar el inconveniente que no encuentra registro de orden basta
+        //tengo aqui codificacion ,que si Diosito quiere va a funcionar
+        //------------------------------------------------------------------------
+
+    }//buscar
+}//class
--- Proyecto/src/Orden/ItemOrden.java
+++ Proyecto/src/Orden/ItemOrden.java
@@ -25,7 +25,6 @@ public class ItemOrden {
 
     @Override
     public String toString(){
-
         return "";
     }
     
